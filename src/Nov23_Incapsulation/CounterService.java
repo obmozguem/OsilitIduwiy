cf996c7751a69674765599105bd8a547228bfafa
Your Name <oleckaask@gmail.com>
2022-11-24 21:40:49 +0300

    Prostie cisla

--- src/Nov23_Incapsulation/CounterService.java
+++ src/Nov23_Incapsulation/CounterService.java
@@ -1,15 +1,27 @@
 package Nov23_Incapsulation;
+
 import java.util.Arrays;
 /*Также реализовать сервис CounterService, зона ответственности которого — хранение массива доступных счетчиков,
 получение всех доступных счетчиков, получение доступа к счетчику по названию, увеличение значения счетчика на
 единицу или заданное значение, а также сброс счетчика до нулевого значения.*/
 
- public class CounterService {
-    public void arrayCounters (Counters... array){
+public class CounterService {
+    public void arrayCounters(Counters... array) {
         System.out.println(Arrays.toString(array));
     }
-    public int increase(Counters, int value) {
 
+    public void increase(Counters count, int value) {
+        int newCount = count.getCounter() + value;
+        count.setCounter(newCount);
+    }
 
+    public void increment(Counters count) {
+        int newCount = count.getCounter() + 1;
+        count.setCounter(newCount);
+    }
+
+    public void zeroing(Counters count) {
+        count.setCounter(0);
     }
 }
+// ОСТАЛОСЬ ПО НАЗВАНИЮ!
